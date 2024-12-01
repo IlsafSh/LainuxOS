619c0d904a0a0e75a068bf832bc553f3b61f4651
Nortero <sharibzhanov.ilsaf@mail.ru>
2024-12-01 19:02:27 +0300

    Layer 04. Ports and Printf
    Interacting with ports and improving printf

--- kernel.cpp
+++ kernel.cpp
@@ -4,9 +4,37 @@
 void printf(char* str)
 {
     static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+    // Cursor point
+    static uint8_t x=0,y=0;
 
+    // Screan = 80x25
     for (int i=0; str[i] != '\0'; ++i) {
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+        switch(str[i])
+        {
+            case '\n':
+                x = 0;
+                y++;
+                break;
+            default:
+                VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | str[i];
+                x++;
+                break;
+        }
+        // Linefeed
+        if(x >= 80)
+        {
+            x = 0;
+            y++;
+        }
+        // Clean a filled screan
+        if(y >= 25)
+        {
+            for(y = 0; y < 25; y++)
+                for(x = 0; x < 80; x++)
+                    VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
+            x = 0;
+            y = 0;
+        }
     }
 }
 
@@ -21,7 +49,9 @@ extern "C" void callConstructors()
 
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot_magic_number*/)
 {
-    printf("Let's all love Lain!");
+    printf("Let's all love Lain!\n");
+    printf("Present Day, Present Time\n");
+    printf("HAHAHAHAHA...");
 
     GlobalDescriptorTable gdt;
 
